1795267db57101564599f70e7b746318a866aa08
Ferry Chahaya <F3rry.Chahaya@hotmail.com>
2025-06-14 15:39:43 -0500

    Add initial setup for movie-weather graphql app

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+import express from 'express';
+
+const app = express();
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server ready at http://localhost:${PORT}`);
+});
